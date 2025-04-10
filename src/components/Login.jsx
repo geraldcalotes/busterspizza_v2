948494da5c7b9319caf9ce691152ab936f862ade
Gerald Calotes <gerald.calotes.dev@gmail.com>
2025-04-10 15:39:09 -0600

    remove testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -62,7 +62,6 @@ function Login() {
     <div className="login-container">
       <div className="login-box">
         <div className="logo-container">
-          <h3>testing only</h3>
           <img src={logo} alt="Company Logo" className="login-logo" />
         </div>
         <div className="form-group store-selector">
